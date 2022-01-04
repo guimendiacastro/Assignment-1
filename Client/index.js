8affe9a82b3fb281366d95ad2b6b0800ea719a7e
Gui Castro <jcwm68@durham.ac.uk>
2022-01-04 19:02:11 +0000

    Can now search the json of games by team name

--- Client/index.js
+++ Client/index.js
@@ -1,3 +1,4 @@
+/*
 window.addEventListener('click', function(event){
     fetch('http://127.0.0.1:8090/list')
       .then(function(response ){
@@ -10,5 +11,51 @@ window.addEventListener('click', function(event){
       .then(body => document.getElementById('content').innerHTML=body)
       .catch( (error) => alert("Page not found, try again later"))
   });
+*/
+/*
+const form = document.getElementById('form1');
+const tests = document.getElementById('test');
 
+form.addEventListener('submit', function(event){
+   
+  fetch('http://127.0.0.1:8090/table')
+    .then(function(response ){
+      if(response.ok){
+        
+        r = response.text();
+        return response.text();
+      } else {
+        throw new Error("Page not found, try again later");
+      }
+    })
+    .then(body => document.getElementById('test').innerHTML=r)
+    .catch( (error) => alert("Page not found, try again later"))
+  
+    event.preventDefault();
+    
+});
+*/
 
+const qf = document.getElementById("bball_question_form");
+
+qf.addEventListener('submit', async function(event){
+  // stop the form from being submitted
+  event.preventDefault();
+  // extract data from form
+  const data = new FormData(qf); 
+  // prepare data for GET query parameters
+  const params= new URLSearchParams(data);
+  // concatenating params will call .toString()
+  const response = await fetch('http://127.0.0.1:8090/bball/question?' + params);
+  // use response.json() to parse the body into an object
+  // otherwise could use JSON.parse()
+  const testing = await response.json();
+
+  const ql = document.getElementById('timetable_content_list');
+  let lis = "";
+  for (let bball of testing){  // assuming goats is a list
+    // backtick expressions are helpful for simple templating
+    lis += `<li><b>${bball.team1}</b>: ${bball.team2}`; 
+  }
+  ql.innerHTML = lis;
+});
--- server.js
+++ server.js
@@ -1,12 +1,62 @@
 const express = require('express');
 const app = express();
 
+
 app.use(express.static('Client'));
 
+var testing = require('./games-timetable.json');
 let instruments = [ 'piano', 'concertina', 'double bas'];
 
 app.get('/list', function (req, resp){
     resp.send(instruments);
 });
+
+
+app.get('/table', function (req, resp) {
+    const search = req.query.search;
+    let results = [];
+  
+    for (let i = 0; i < timetables.length; i++) {
+      let timetable = timetables[i];
+      if (timetable.team1.includes(search)) {
+        results.push(timetable.score);
+      }
+    }
+    resp.send(results);
+  });
+
+
+  const goats = [
+    {
+      name: 'Phil',
+      fact: 'Stars in Hercules'
+    },
+    {
+      name: 'Billy',
+      fact: 'Stars in Stardust'
+    },
+    {
+      name: 'Mr Tumnus',
+      fact: 'Features in The Lion, the Witch and the Wardrobe'
+    },
+    {
+      name: 'Jocelyn Bell Burnell',
+      fact: 'Discovered pulsars'
+    }
+  ];
+  
+
+
+
+  app.get('/bball/question', function (req, resp) {
+    const q = req.query.bball_timetable;
+    const answers = [];
+    for (const bball of testing) {
+      if (bball.team1.includes(q)) {
+        answers.push(bball);
+      }
+    }
+    resp.json(answers); // resp.send would also send as JSON
+  });
 
 app.listen(8090);
