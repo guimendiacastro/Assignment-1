1d8cb4edd7ac2113afade738e5d3ab87f266e1e9
Gui Castro <jcwm68@durham.ac.uk>
2022-01-05 21:10:32 +0000

    Added post request in admin page

--- server.js
+++ server.js
@@ -1,53 +1,23 @@
 const express = require('express');
 const app = express();
+var fs = require('fs');
+var data = fs.readFileSync('games-timetable.json');
+var games = JSON.parse(data)
 
 
+app.use(express.urlencoded());
 app.use(express.static('Client'));
 
 var testing = require('./games-timetable.json');
 let instruments = [ 'piano', 'concertina', 'double bas'];
 
+
+
 app.get('/list', function (req, resp){
     resp.send(instruments);
 });
 
-
-app.get('/table', function (req, resp) {
-    const search = req.query.search;
-    let results = [];
   
-    for (let i = 0; i < timetables.length; i++) {
-      let timetable = timetables[i];
-      if (timetable.team1.includes(search)) {
-        results.push(timetable.score);
-      }
-    }
-    resp.send(results);
-  });
-
-
-  const goats = [
-    {
-      name: 'Phil',
-      fact: 'Stars in Hercules'
-    },
-    {
-      name: 'Billy',
-      fact: 'Stars in Stardust'
-    },
-    {
-      name: 'Mr Tumnus',
-      fact: 'Features in The Lion, the Witch and the Wardrobe'
-    },
-    {
-      name: 'Jocelyn Bell Burnell',
-      fact: 'Discovered pulsars'
-    }
-  ];
-  
-
-
-
   app.get('/bball/question', function (req, resp) {
     const q = req.query.bball_timetable;
     const answers = [];
@@ -58,5 +28,22 @@ app.get('/table', function (req, resp) {
     }
     resp.json(answers); // resp.send would also send as JSON
   });
+
+
+
+app.post('/timetable/new', function(req, resp){
+  console.log(req.body);
+  new_game = req.body;
+  games.push(new_game);
+  var data = JSON.stringify(games, null, 2);
+  console.log(data);
+  fs.writeFile('games-timetable.json', data, finished);
+  function finished(err){
+    console.log("All set ")
+    resp.send("Updated")
+  }
+  
+  
+});
 
 app.listen(8090);
